06aa129ed259fcfe6ad0182adf288d2d828b8a2a
Carlos Adriano Portes <xcap2000@outlook.com>
2022-10-18 20:54:23 -0300

    Add todo

--- src/DataTools/Calculator/Mappers/SumRequestMapper.cs
+++ src/DataTools/Calculator/Mappers/SumRequestMapper.cs
@@ -15,6 +15,8 @@ public class SumRequestMapper : ISumRequestMapper
     public ISumRequestReader Map(int value1, int value2)
     {
         var request = factory.Create();
+        // TODO - Remove this pattern, add getters and setters to the interface or create an always valid controller.
+        // TODO - Remember that we can use reflection to set properties.
         var writer = request as ISumRequestWriter;
         writer.Value1 = value1;
         writer.Value2 = value2;

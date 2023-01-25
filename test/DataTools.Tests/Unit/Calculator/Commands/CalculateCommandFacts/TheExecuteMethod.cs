6592218e6bce8563fb96eafa2c78001443b893ff
Carlos Adriano Portes <xcap2000@outlook.com>
2023-01-24 22:22:14 -0300

    Start calculate command tests

---
+++ test/DataTools.Tests/Unit/Calculator/Commands/CalculateCommandFacts/TheExecuteMethod.cs
@@ -0,0 +1,19 @@
+namespace CarpeDiem.DataTools.Unit.Calculator.Commands;
+
+public static partial class CalculateCommandFacts
+{
+    public class TheExecuteMethod : BaseFacts
+    {
+        [Fact]
+        public void PublishesActivatedEvent()
+        {
+            // Given
+
+            // When
+            // TODO - Implement.
+            // command.Execute()
+
+            // Then
+        }
+    }
+}

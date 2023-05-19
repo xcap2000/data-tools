490fc26340e18ab5dd185cb84ef23bb1fcc1b485
Carlos Adriano Portes <xcap2000@outlook.com>
2023-05-18 22:33:31 -0300

    Add close button

--- src/DataTools/Calculator/Views/CalculatorView.Designer.cs
+++ src/DataTools/Calculator/Views/CalculatorView.Designer.cs
@@ -28,127 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.messageLabel = new System.Windows.Forms.Label();
-            this.sumButton = new System.Windows.Forms.Button();
-            this.resultLabel = new System.Windows.Forms.Label();
-            this.value2TextBox = new System.Windows.Forms.TextBox();
-            this.value1TextBox = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            messageLabel = new System.Windows.Forms.Label();
+            sumButton = new System.Windows.Forms.Button();
+            resultLabel = new System.Windows.Forms.Label();
+            value2TextBox = new System.Windows.Forms.TextBox();
+            value1TextBox = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            errorProvider = new System.Windows.Forms.ErrorProvider(components);
+            closeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)errorProvider).BeginInit();
+            SuspendLayout();
             // 
             // messageLabel
             // 
-            this.messageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.messageLabel.ForeColor = System.Drawing.Color.Red;
-            this.messageLabel.Location = new System.Drawing.Point(12, 135);
-            this.messageLabel.Name = "messageLabel";
-            this.messageLabel.Size = new System.Drawing.Size(760, 23);
-            this.messageLabel.TabIndex = 15;
-            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            messageLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            messageLabel.ForeColor = System.Drawing.Color.Red;
+            messageLabel.Location = new System.Drawing.Point(12, 135);
+            messageLabel.Name = "messageLabel";
+            messageLabel.Size = new System.Drawing.Size(760, 23);
+            messageLabel.TabIndex = 15;
+            messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // sumButton
             // 
-            this.sumButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.sumButton.AutoSize = true;
-            this.sumButton.Location = new System.Drawing.Point(697, 526);
-            this.sumButton.Name = "sumButton";
-            this.sumButton.Size = new System.Drawing.Size(75, 25);
-            this.sumButton.TabIndex = 14;
-            this.sumButton.Text = "Sum";
-            this.sumButton.UseVisualStyleBackColor = true;
+            sumButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            sumButton.AutoSize = true;
+            sumButton.Location = new System.Drawing.Point(616, 524);
+            sumButton.Name = "sumButton";
+            sumButton.Size = new System.Drawing.Size(75, 25);
+            sumButton.TabIndex = 14;
+            sumButton.Text = "Sum";
+            sumButton.UseVisualStyleBackColor = true;
             // 
             // resultLabel
             // 
-            this.resultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.resultLabel.Location = new System.Drawing.Point(12, 112);
-            this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(760, 23);
-            this.resultLabel.TabIndex = 13;
-            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            resultLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            resultLabel.Location = new System.Drawing.Point(12, 112);
+            resultLabel.Name = "resultLabel";
+            resultLabel.Size = new System.Drawing.Size(760, 23);
+            resultLabel.TabIndex = 13;
+            resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // value2TextBox
             // 
-            this.value2TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.value2TextBox.Location = new System.Drawing.Point(12, 71);
-            this.value2TextBox.Name = "value2TextBox";
-            this.value2TextBox.PlaceholderText = "Enter Value 2";
-            this.value2TextBox.Size = new System.Drawing.Size(760, 23);
-            this.value2TextBox.TabIndex = 12;
+            value2TextBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            value2TextBox.Location = new System.Drawing.Point(12, 71);
+            value2TextBox.Name = "value2TextBox";
+            value2TextBox.PlaceholderText = "Enter Value 2";
+            value2TextBox.Size = new System.Drawing.Size(760, 23);
+            value2TextBox.TabIndex = 12;
             // 
             // value1TextBox
             // 
-            this.value1TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.value1TextBox.Location = new System.Drawing.Point(12, 27);
-            this.value1TextBox.Name = "value1TextBox";
-            this.value1TextBox.PlaceholderText = "Enter Value 1";
-            this.value1TextBox.Size = new System.Drawing.Size(760, 23);
-            this.value1TextBox.TabIndex = 11;
+            value1TextBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            value1TextBox.Location = new System.Drawing.Point(12, 27);
+            value1TextBox.Name = "value1TextBox";
+            value1TextBox.PlaceholderText = "Enter Value 1";
+            value1TextBox.Size = new System.Drawing.Size(760, 23);
+            value1TextBox.TabIndex = 11;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 97);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 15);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Result:";
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(12, 97);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(42, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Result:";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 53);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 15);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Value 2:";
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(12, 53);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(47, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Value 2:";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 15);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Value 1:";
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(47, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Value 1:";
             // 
             // errorProvider
             // 
-            this.errorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
-            this.errorProvider.ContainerControl = this;
+            errorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            errorProvider.ContainerControl = this;
+            // 
+            // closeButton
+            // 
+            closeButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            closeButton.AutoSize = true;
+            closeButton.Location = new System.Drawing.Point(697, 524);
+            closeButton.Name = "closeButton";
+            closeButton.Size = new System.Drawing.Size(75, 25);
+            closeButton.TabIndex = 16;
+            closeButton.Text = "Close";
+            closeButton.UseVisualStyleBackColor = true;
+            closeButton.Click += closeButton_Click;
             // 
             // CalculatorView
             // 
-            this.AcceptButton = this.sumButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.messageLabel);
-            this.Controls.Add(this.sumButton);
-            this.Controls.Add(this.resultLabel);
-            this.Controls.Add(this.value2TextBox);
-            this.Controls.Add(this.value1TextBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.MinimumSize = new System.Drawing.Size(800, 600);
-            this.Name = "CalculatorView";
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
-            this.Text = "Calculator";
-            this.Load += new System.EventHandler(this.WinFormsMiniCalcView_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AcceptButton = sumButton;
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            CancelButton = closeButton;
+            ClientSize = new System.Drawing.Size(784, 561);
+            Controls.Add(closeButton);
+            Controls.Add(messageLabel);
+            Controls.Add(sumButton);
+            Controls.Add(resultLabel);
+            Controls.Add(value2TextBox);
+            Controls.Add(value1TextBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MinimumSize = new System.Drawing.Size(800, 600);
+            Name = "CalculatorView";
+            SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            Text = "Calculator";
+            Load += WinFormsMiniCalcView_Load;
+            ((System.ComponentModel.ISupportInitialize)errorProvider).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -162,5 +172,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Button closeButton;
     }
 }
--- src/DataTools/Calculator/Views/CalculatorView.cs
+++ src/DataTools/Calculator/Views/CalculatorView.cs
@@ -63,4 +63,9 @@ public partial class CalculatorView : Form, ICalculatorView
             Hide();
         }
     }
+
+    private void closeButton_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
 }
